738623b9ad0734b7ae4d0f8852d414e5b96371f2
admin <admin@admins-MBP.attlocal.net>
2019-05-27 12:06:36 -0700

    Integrated index action in bookstores

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-10.times do |bookstore|
+15.times do |bookstore|
   Bookstore.create!(
     name: "Bookstore #{bookstore}",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -7,7 +7,7 @@
   )
 end
 
-puts "10 bookstores created"
+puts "15 bookstores created"
 
 5.times do |quiz|
   Quiz.create!(
