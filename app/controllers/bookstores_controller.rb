b67c4a9742aa39f5fe1444960174dff58634c458
admin <admin@admins-MBP.attlocal.net>
2019-05-27 12:50:43 -0700

    Integrated new and create actions for bookstores

--- app/controllers/bookstores_controller.rb
+++ app/controllers/bookstores_controller.rb
@@ -2,4 +2,26 @@ class BookstoresController < ApplicationController
   def index
     @bookstores = Bookstore.all
   end
+
+  def new
+    @bookstore = Bookstore.new
+  end
+    
+  def create
+    @bookstore = Bookstore.new(bookstore_params)
+
+    respond_to do |format|
+      if @bookstore.save
+        format.html { redirect_to bookstores_path, notice: 'Your bookstore is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+    def bookstore_params
+      params.require(:bookstore).permit(:name, :description, :latitude, :longitude)
+    end
 end
