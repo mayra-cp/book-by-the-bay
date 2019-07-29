74048e80cb6379e448c115c65ed1d30bab9478e0
Mayra-io <admin@admins-MBP.attlocal.net>
2019-07-29 16:08:38 -0700

    customized bookstore routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :bookstores
+  resources :bookstores, except: [:show]
+  get 'bookstore/:id', to: 'bookstores#show', as: 'bookstore_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
